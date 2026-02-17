ad303df2e26da7232e8cf13ab2615cd6774a79e4
Ben Tomlinson <53353052+benepixx@users.noreply.github.com>
2026-02-17 09:29:44 +0000

    Create SonosFavoritesModal component

---
+++ src/modals/SonosFavoritesModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SonosFavoritesModal = () => {
+    return (
+        <div className="sonos-favorites-modal">
+            <h2>Sonos Favorites</h2>
+            {/* Implement the modal content here */}
+        </div>
+    );
+};
+
+export default SonosFavoritesModal;
